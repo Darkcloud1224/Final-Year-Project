615df64075337235581c6df139bae33a2acbd29c
Darkcloud1224 <127832687+Darkcloud1224@users.noreply.github.com>
2024-06-27 01:35:10 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+      if (env('APP_ENV') === 'production') {
+          $this->app['request']->server->set('HTTPS', true);
+      }
         Schema::defaultStringLength(191);
 
         View::composer('*', function ($view) {
